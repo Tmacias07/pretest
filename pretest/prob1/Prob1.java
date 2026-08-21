476cf88bcffb72c52df9ca23fe058b1f8dad49ff
Tristan Macias <tmacia100@uafs.edu>
2026-08-21 09:46:47 -0500

    pretest

--- pretest/prob1/Prob1.java
+++ pretest/prob1/Prob1.java
@@ -1,9 +1,34 @@
 package prob1;
 
 public class Prob1 {
+	public static int findMaxDifference(int[] nums) {
+		if(nums.length < 2) {
+			return 0;
+		}
+		int maxdiff = 0;
+		int tempNum1;
+		int tempNum2;
+		for(int i = 0; i < nums.length & i + 1 != nums.length; i++) {
+			tempNum1 = nums[i];
+			System.out.print(nums[i] + " ");
+			
+			tempNum2 = nums[i+1];
+			System.out.println(nums[i+1]);
+			
+			if(Math.abs(tempNum1 - tempNum2) > maxdiff) {
+				maxdiff = Math.abs(tempNum1 - tempNum2);
+			}
+		}
+		return maxdiff;
+
+		
+	}
 
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
+		int[] nums = {4,7,2,9,1};
+		System.out.println();
+		int maxDiff = findMaxDifference(nums);
+		System.out.println("The max differences in the array is " + maxDiff);
 
 	}
 
--- pretest/prob2/Prob2.java
+++ pretest/prob2/Prob2.java
@@ -1,9 +1,33 @@
 package prob2;
 
 public class Prob2 {
+	public static int countVowelSequences(String str) {
+		int lastVowel = 0;
+		int seqCount = 0;
+		String[] vowels = { "a","e","i","o","u"};
+		
+		for(int i = 0; i < str.length(); i++) {
+			String temp = str.charAt(i) +"";
+			for(int j = 0; j < vowels.length; j++) {
+				if(temp.equals(vowels[j])) {
+					if(lastVowel == 1) {
+						seqCount++;
+						lastVowel = 0;
+					}
+					else {
+						lastVowel = 0;
+					}
+					
+				}
+			}	
+		}
+		return seqCount;
+	}
 
 	public static void main(String[] args) {
-		
+		String str = "Steam engine in heavy rain";
+		System.out.println("There are " + countVowelSequences(str) + " Vowels in sequence");
+		// couldnt complete :(
 
 	}
 
